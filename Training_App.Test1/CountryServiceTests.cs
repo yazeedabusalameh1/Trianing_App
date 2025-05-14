da3d343d2a7e5a079d3ca373d31f29761c2f78e1
Yazeed Abusalameh <yazeedabusalamh2002@gmail.com>
2025-05-14 14:33:57 +0300

    Unit Tese V1

---
+++ Training_App.Test1/CountryServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using DAL.Data;
+using DAL.ModelsDAL;
+using DAL.RepositoryDAL;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_App.Tests
+{
+    [TestClass]
+    public class CityRepositoryDALTests
+    {
+        [TestMethod]
+        public void GivenValidCity_WhenInsertCity_ThenReturnsTrue()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<DBContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB")
+                .Options;
+
+            using var context = new DBContext(options);
+            var repo = new CityRepositoryDAL(context);
+
+            var city = new CityDAL
+            {
+                CityID = 1,
+                CityName = "Jenin",
+                Population = 100000,
+                Governorate = "Jenin",
+                Country = "Palestine",
+                CityRank = 1
+            };
+
+            // Act
+            var result = repo.InsertCity(city);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(1, context.Citys.Count());
+        }
+    }
+}
---
+++ Training_App.test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Training_App.test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
