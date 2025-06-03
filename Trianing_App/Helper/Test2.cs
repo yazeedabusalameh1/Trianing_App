b7017013f8920781a4fc29ef056184d273513b39
Yazeed Abusalameh <yazeedabusalamh2002@gmail.com>
2025-06-03 14:07:05 +0300

    Test 2 Say hi into amara

---
+++ Trianing_App/Helper/Test2.cs
@@ -0,0 +1,10 @@
+﻿namespace Trianing_App.Helper
+{
+    public class Test2
+    {
+        public void Say_Hi_Into_Amara()
+        {
+            Console.WriteLine("Hi Amara");
+        }
+    }
+}
