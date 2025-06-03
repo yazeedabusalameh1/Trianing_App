18629f92ce92291e423066272a630f5b320accc8
Yazeed Abusalameh <yazeedabusalamh2002@gmail.com>
2025-06-03 14:37:16 +0300

    new Chamge (Rename)

---
+++ Trianing_App/Helper/testTow.cs
@@ -0,0 +1,10 @@
+﻿namespace Trianing_App.Helper
+{
+    public class testTow
+    {
+        public void Say_Hi_Into_Amara()
+        {
+            Console.WriteLine("Hi Amara");
+        }
+    }
+}
