4a151ee1f9227b297e93edfedc6f5ea0e428cdd6
Amara-Rabaya <rabayaamara1@gmail.com>
2025-06-03 12:16:41 +0300

    ..

---
+++ Trianing_App/Helper/TestHelperForGit.cs
@@ -0,0 +1,12 @@
+﻿namespace Trianing_App.Helper
+{
+    public class TestHelperForGit
+    {
+        public static void Hello_Yazeed()
+        {
+            Console.WriteLine("Hello Yazeed");
+        }
+        public static void AunserYazeed() {  
+            Console.WriteLine("Hi Yazeed"); }
+    }
+}
