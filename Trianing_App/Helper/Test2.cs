3a5c3aebc0f9fc1ad1244bf1e35d0276eb2145ba
Amara-Rabaya <rabayaamara1@gmail.com>
2025-06-03 14:07:05 +0300

    hi yazeed

---
+++ Trianing_App/Helper/Test2.cs
@@ -0,0 +1,10 @@
+﻿namespace Trianing_App.Helper
+{
+    public class Test2
+    {
+        public void say_hi_in_Yazeed()
+        {
+            Console.WriteLine("hi Yazeed");
+        }
+    }
+}
