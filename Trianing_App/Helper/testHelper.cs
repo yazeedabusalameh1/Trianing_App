fb7229ddf1daf6720fc9a71eee9fad52adc58cee
Yazeed Abusalameh <yazeedabusalamh2002@gmail.com>
2025-06-03 12:15:20 +0300

    ++

---
+++ Trianing_App/Helper/testHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Trianing_App.Helper
+{
+    public class testHelper
+    {
+        public static void Hello_Amara()
+        {
+            Console.WriteLine("Hello Amara");
+        }
+        public void Answer_From_Yazeed()
+        {
+            Console.WriteLine("Wlecome Amarah");
+        }
+    }
+}
